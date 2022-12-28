2165228c1efee3d005a39617ddda6e210d77b3bf
Vandré Willian Priaro <vandrepriaro1@gmail.com>
2022-12-27 21:24:29 -0300

    Adicionar arquivos de projeto.

---
+++ WebCrash.Testes/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WebCrash.Testes")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WebCrash.Testes")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9915d714-c61c-4850-9507-f4ae6a629cf6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WebCrash.WebScraping/Main.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace WebCrash.WebScraping
+{
+    public class Main
+    {
+        public void Configurar()
+        {
+            new DriverManager().SetUpDriver(new ChromeConfig());
+
+            var driver = new ChromeDriver();
+        }
+    }
+}
